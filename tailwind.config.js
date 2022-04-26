b2a1f480954e10e71712941ce358bdbaba779df4
LeptoFlare <47790183+LeptoFlare@users.noreply.github.com>
2022-04-25 23:15:43 -0400

    Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        theme: {
+          space: "#1E1E50",
+          night: "#28285A",
+          blue: "#5050FF",
+          lightblue: "#B1B1FF",
+          pink: "#FF0066",
+          lightpink: "#FF6699",
+          lime: "#99FF00",
+          lightlime: "#B6FF61",
+          inchworm: "#B6FF61",
+          white: "#F5F5FF",
+        },
+      },
+      fontFamily: {
+        sans: ["Fredoka", ...defaultTheme.fontFamily.sans],
+        mono: ["Audiowide", ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
-};
+}
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
   base: "./",
   build: {
+    sourcemap: true,
     outDir: "graphics",
     rollupOptions: {
       input: {
